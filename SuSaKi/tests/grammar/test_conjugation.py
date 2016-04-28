dfb6a6bdcfbc9560c4d7b599d1df050f55e888af
Simon Clement <simon.clement+git@gmail.com>
2016-04-28 16:18:31 +0300

    Create test skeleton

---
+++ SuSaKi/tests/grammar/test_conjugation.py
@@ -0,0 +1,41 @@
+'''
+Created on Apr 28, 2016
+
+@author: simon
+'''
+import pytest
+
+from susaki.grammar.conjugation import VerbConjugator, KPTChanger
+
+
+class VerbConjugatorTest:
+
+    def test_can_conjugate_verb_type_1_correctly_in_present_tense(self):
+        assert False
+
+    def test_can_conjugate_verb_type_2_correctly_in_present_tense(self):
+        assert False
+
+    def test_can_conjugate_verb_type_3_correctly_in_present_tense(self):
+        assert False
+
+    def test_can_conjugate_verb_type_4_correctly_in_present_tense(self):
+        assert False
+
+    def test_can_conjugate_verb_type_5_correctly_in_present_tense(self):
+        assert False
+
+    def test_can_conjugate_verb_type_6_correctly_in_present_tense(self):
+        assert False
+
+
+class TestKPTChanger:
+
+    def test_can_find_correct_strong_kpt_patterns(self):
+        assert False
+
+    def test_can_find_correct_weak_kpt_patterns(self):
+        assert False
+
+    def test_can_deal_with_diabolical_k(self):
+        assert False
