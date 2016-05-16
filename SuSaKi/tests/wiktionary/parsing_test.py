331b722a861c96f5e27432d9db6f93497bf46bd7
Simon Clement <simon.clement+git@gmail.com>
2016-05-16 17:10:25 +0300

    Add note to one test

--- SuSaKi/tests/wiktionary/parsing_test.py
+++ SuSaKi/tests/wiktionary/parsing_test.py
@@ -11,9 +11,6 @@ class TestHTMLParser:
     def test_only_returns_article_in_correct_target_language(self):
         assert False
 
-    def test_can_translate_from_english_to_target_language(self):
-        assert False
-
     def test_returns_synonyms_if_they_exists(self):
         assert False
 
@@ -43,3 +40,8 @@ class TestHTMLParser:
 
     def test_returns_declensions_table_if_it_exists(self):
         return False
+
+#
+#     #Should be moved to another class
+#     def test_can_translate_from_english_to_target_language(self):
+#         assert False
