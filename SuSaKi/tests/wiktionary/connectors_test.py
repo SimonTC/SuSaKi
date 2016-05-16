ca7814655071c866eb3db49b7d634eb3820802dd
Simon Clement <simon.clement+git@gmail.com>
2016-05-16 17:04:34 +0300

    FInnished html connector tests

--- SuSaKi/tests/wiktionary/connectors_test.py
+++ SuSaKi/tests/wiktionary/connectors_test.py
@@ -5,12 +5,13 @@ Created on May 12, 2016
 '''
 import pytest
 import requests
+from requests_file import FileAdapter
 
 import os
 from distutils import dir_util
 
 from susaki.wiktionary.connectors import HTMLConnector
-from _pytest.monkeypatch import monkeypatch
+from unittest.mock import MagicMock, patch
 
 
 @pytest.fixture
@@ -30,60 +31,38 @@ def datadir(tmpdir, request):
     return tmpdir
 
 
-class HTMLConnectorTest:
+class TestHTMLConnector:
 
     @pytest.fixture
     def connector(self):
         return HTMLConnector('Finnish')
 
-    def test_returns_error_when_word_is_completely_unknown(self, connector, datadir):
-        def mock_unknown_page(word):
-            page_path = '/'.join([str(datadir), 'no_result.html'])
-            return requests.get(page_path)
-        monkeypatch.setattr(HTMLConnector, '_collect_page', mock_unknown_page)
-        assert connector.collect_raw_article('sää') == False
+    @pytest.fixture
+    def request_session(self):
+        s = requests.Session()
+        s.mount('file://', FileAdapter())
+        return s
 
-    def test_returns_suggestions_when_the_word_doesnt_have_an_article_but_exists_in_other_article(self):
-        assert False
+    @patch('susaki.wiktionary.connectors.HTMLConnector._collect_page')
+    def test_returns_KeyError_when_word_is_completely_unknown(self, mock_collector, connector, datadir, request_session):
+        page_path = '/'.join([str(datadir), 'no_result.html'])
+        mock_collector.return_value = request_session.get(
+            'file://' + page_path)
+        with pytest.raises(KeyError):
+            connector.collect_raw_article('')
 
-    def test_returns_article_when_word_has_an_article(self):
-        assert False
+    @patch('susaki.wiktionary.connectors.HTMLConnector._collect_page')
+    def test_returns_suggestions_when_the_word_doesnt_have_an_article_but_exists_in_other_article(self, mock_collector, connector, datadir, request_session):
+        page_path = '/'.join([str(datadir), 'multiple_suggestions.html'])
+        mock_collector.return_value = request_session.get(
+            'file://' + page_path)
+        result = connector.collect_raw_article('')
+        assert type(result) is list
 
-# Following tests should be in other class since they are about the
-# parsing of the page and not retrieval
-
-    def test_only_returns_article_in_correct_target_language(self):
-        assert False
-
-    def test_can_translate_from_english_to_target_language(self):
-        assert False
-
-    def test_returns_synonyms_if_they_exists(self):
-        assert False
-
-    def test_returns_derived_terms_if_they_exists(self):
-        assert False
-
-    def test_returns_none_as_synonym_when_no_synonyms_exists(self):
-        assert False
-
-    def test_returns_none_as_derived_term_when_no_derived_terms_exist(self):
-        assert False
-
-    def test_returns_correct_definitions_of_a_given_word(self):
-        assert False
-
-    def test_returns_the_correct_translations_of_the_definitions(self):
-        assert False
-
-    def test_returns_correct_examples_for_each_translation(self):
-        assert False
-
-    def test_returns_compound_words_if_they_exists(self):
-        return False
-
-    def test_returns_none_as_compund_words_if_none_exists(self):
-        return False
-
-    def test_returns_declensions_table_if_it_exists(self):
-        return False
+    @patch('susaki.wiktionary.connectors.HTMLConnector._collect_page')
+    def test_returns_article_when_word_has_an_article(self, mock_collector, connector, datadir, request_session):
+        page_path = '/'.join([str(datadir), 'article_exists.html'])
+        mock_collector.return_value = request_session.get(
+            'file://' + page_path)
+        result = connector.collect_raw_article('')
+        assert type(result) is requests.models.Response
