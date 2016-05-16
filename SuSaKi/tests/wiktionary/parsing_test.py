8edaa2c801f1349c7f653157f86359a0213bc7a2
Simon Clement <simon.clement+git@gmail.com>
2016-05-16 17:04:54 +0300

    Move test parsing skeleton to own class

---
+++ SuSaKi/tests/wiktionary/parsing_test.py
@@ -0,0 +1,45 @@
+'''
+Created on May 16, 2016
+
+@author: simon
+'''
+import pytest
+
+
+class TestHTMLParser:
+
+    def test_only_returns_article_in_correct_target_language(self):
+        assert False
+
+    def test_can_translate_from_english_to_target_language(self):
+        assert False
+
+    def test_returns_synonyms_if_they_exists(self):
+        assert False
+
+    def test_returns_derived_terms_if_they_exists(self):
+        assert False
+
+    def test_returns_none_as_synonym_when_no_synonyms_exists(self):
+        assert False
+
+    def test_returns_none_as_derived_term_when_no_derived_terms_exist(self):
+        assert False
+
+    def test_returns_correct_definitions_of_a_given_word(self):
+        assert False
+
+    def test_returns_the_correct_translations_of_the_definitions(self):
+        assert False
+
+    def test_returns_correct_examples_for_each_translation(self):
+        assert False
+
+    def test_returns_compound_words_if_they_exists(self):
+        return False
+
+    def test_returns_none_as_compund_words_if_none_exists(self):
+        return False
+
+    def test_returns_declensions_table_if_it_exists(self):
+        return False
