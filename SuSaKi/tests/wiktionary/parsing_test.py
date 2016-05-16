5680dfcd70117f9fb33a67424340fa33308aa375
Simon Clement <simon.clement+git@gmail.com>
2016-05-16 22:51:32 +0300

    Setup first test

--- SuSaKi/tests/wiktionary/parsing_test.py
+++ SuSaKi/tests/wiktionary/parsing_test.py
@@ -4,9 +4,46 @@ Created on May 16, 2016
 @author: simon
 '''
 import pytest
+import os
+from distutils import dir_util
+import requests
+from requests_file import FileAdapter
+from susaki.wiktionary.parsing import HTMLParser, Article
+
+
+@pytest.fixture
+def datadir(tmpdir, request):
+    '''
+    Fixture responsible for searching a folder with the same name of test
+    module and, if available, moving all contents to a temporary directory so
+    tests can use them freely.
+    Source: http://stackoverflow.com/a/29631801
+    '''
+    filename = request.module.__file__
+    test_dir, _ = os.path.splitext(filename)
+
+    if os.path.isdir(test_dir):
+        dir_util.copy_tree(test_dir, str(tmpdir))
+
+    return tmpdir
 
 
 class TestHTMLParser:
+
+    @pytest.fixture
+    def raw_article(self, datadir):
+        article_path = '/'.join([str(datadir), 'kuu.html'])
+        s = requests.Session()
+        s.mount('file://', FileAdapter())
+        return s.get('file://' + article_path)
+
+    @pytest.fixture
+    def parser(self):
+        return HTMLParser('Finnish')
+
+    def test_returns_article_object(self, parser, raw_article):
+        result = parser.parse_article(raw_article, 'kuu')
+        assert isinstance(result, Article)
 
     def test_only_returns_article_in_correct_target_language(self):
         assert False
