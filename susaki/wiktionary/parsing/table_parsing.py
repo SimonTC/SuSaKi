3eca3ffc59af39aba99f0a9b5727671650f7239b
Simon Clement <simon.clement+git@gmail.com>
2016-08-09 14:47:22 +0300

    Create own class for noun table parsing

--- susaki/wiktionary/parsing/table_parsing.py
+++ susaki/wiktionary/parsing/table_parsing.py
@@ -49,3 +49,86 @@ class TableParser:
         word, kotus_type, kotus_word, gradation = self._extract_meta_information(headline_text)
         meta_element = self._create_meta_tree(word, kotus_type, kotus_word, gradation)
         return meta_element
+
+    def _clean_text(self, text):
+        clean = text.replace('\n', '')
+        clean = clean.strip()
+        clean = re.sub(r'  *', ' ', clean)
+        return clean
+
+
+class NounTableParser(TableParser):
+
+    def parse_table(self, rows):
+        logger.debug('Inflection table type: Noun')
+        table_root = etree.Element('table')
+        in_accusative = False
+        noun_case_element = None
+
+        start_row = self._find_table_start(rows)
+
+        for i, row in enumerate(rows[start_row:]):
+            logger.debug('Parsing row {}'.format(i + start_row))
+            noun_case_name = row.th.text
+            noun_case_name = self._clean_text(noun_case_name)
+            logger.debug('Creating new noun case element: {}'.format(
+                noun_case_name))
+            noun_case_element = etree.Element(noun_case_name)
+            if in_accusative:
+                logger.debug('Entering second accusative row (genitive)')
+                self._parse_second_accusative_row(noun_case_element, row)
+                in_accusative = False
+            else:
+                table_root.append(noun_case_element)
+                if noun_case_name == 'accusative':
+                    logger.debug('Found the accusative case')
+                    in_accusative = True
+                    noun_case_element = etree.SubElement(
+                        noun_case_element, 'nominative')
+                self._parse_noun_table_row(
+                    row, noun_case_element, noun_case_name)
+        return table_root
+
+    def _find_table_start(self, rows):
+        """ The noun table has to parts. The first part contains nominative,
+        genitive, partitive and illative.
+        After these four cases the main table begins and those four cases
+        are repeated there but not in the same other. Because of this we want to
+        ignore the first lines and first start parsing on the main table.
+        Rows: The rows of the table
+        Returns: the id of the row where the first entry of the main table exists.
+                 (After the table headers)
+        """
+        logger.debug('Starting search for the main table')
+        for i, row in enumerate(rows[1:]):
+            noun_case_name = row.th.text
+            noun_case_name = self._clean_text(noun_case_name)
+            try:
+                etree.Element(noun_case_name)
+            except ValueError as err:
+                if str(err) == 'Empty tag name':
+                    # We found the headers of the real table
+                    logger.debug('Found the table headers')
+                    return i + 1
+                else:
+                    raise
+        raise ValueError("Couldn't find the start of the main table")
+
+    def _parse_second_accusative_row(self, noun_case_element, row):
+        noun_case_element = noun_case_element.getparent()
+        noun_case_element = etree.SubElement(noun_case_element, 'genitive')
+        noun_case_element.text = self._clean_text(row.find('td').text)
+
+    def _parse_noun_table_row(self, row, noun_case_element, noun_case_name):
+        """Extracts the singular and plural form from the table row"""
+        row_elements = row.find_all('td')
+        singular = row_elements[0].text
+        if noun_case_name == 'genitive':
+            logger.debug('Entering genitive case')
+            plural = self._clean_text(row_elements[1].find('span').text)
+        else:
+            plural = self._clean_text(row_elements[1].text)
+        singular_element = etree.SubElement(noun_case_element, 'singular')
+        singular_element.text = self._clean_text(singular)
+        plural_element = etree.SubElement(noun_case_element, 'plural')
+        plural_element.text = self._clean_text(plural)
